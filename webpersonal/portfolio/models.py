28ae13b5a259a41cb24247665be36662def45b7b
Jair Ramón Huerta <jair.17huerta@gmail.com>
2020-05-15 19:50:13 -0500

    sección finalizada

--- webpersonal/portfolio/models.py
+++ webpersonal/portfolio/models.py
@@ -5,9 +5,10 @@ class Project(models.Model):
     title = models.CharField(max_length=200, verbose_name="Titulo")
     description = models.TextField(verbose_name="Descripcion")
     image = models.ImageField(verbose_name="Imagen", upload_to="projects")
+    link = models.URLField(verbose_name="Direccion web", null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True, verbose_name="Fecha de creación")
     updated = models.DateTimeField(auto_now=True, verbose_name="Fecha de edición")
-
+    
     class Meta:
         verbose_name = "Proyecto"
         verbose_name_plural = "Proyectos"
